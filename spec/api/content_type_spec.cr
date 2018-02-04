e0efedb48f2f6eb47233c8580302b6c92d130b02
James Harris <wopian@wopian.me>
2018-02-04 18:33:28 +0000

    test: namespace content_type spec

--- spec/api/content_type_spec.cr
+++ spec/api/content_type_spec.cr
@@ -2,6 +2,6 @@ require "../spec_helper"
 
 describe Hibari do
   it "uses the json content type" do
-    CONTENT_TYPE.should eq "application/vnd.api+json"
+    Hibari::CONTENT_TYPE.should eq "application/vnd.api+json"
   end
 end
