01211f3369393c91114699a06ebf98ecaa8e6fcd
James Harris <wopian@wopian.me>
2018-04-28 03:19:03 +0100

    chore: add content-security-policy, cache-control and server headers

--- src/hibari/headers.cr
+++ src/hibari/headers.cr
@@ -3,10 +3,13 @@ module Hibari
   HEADERS = {
     "Content-Type" => JsonAPI::CONTENT_TYPE,
     "Strict-Transport-Security" => "max-age=63072000; includeSubDomains; preload",
-    "X-Powered-By" => "", # TODO: Remove this header entirely
+    "Content-Security-Policy" => "default-src 'none'",
+    "Cache-Control" => "max-age=86400",
+    "Server" => "api.hibari.moe",
+    "X-Powered-By" => "api.hibari.moe",
     "X-Frame-Options" => "SAMEORIGIN",
-    "X-Xss-Protection" => "1; mode=block",
+    "X-XSS-Protection" => "1; mode=block",
     "X-Content-Type-Options" => "nosniff",
-    "Referrer-Policy" => "strict-origin-when-cross-origin"
+    "Referrer-Policy" => "origin-when-cross-origin, strict-origin-when-cross-origin"
   }
 end
