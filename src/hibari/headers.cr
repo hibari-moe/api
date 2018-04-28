03c7e87ca31b775c6c5bf99e5e63b61c2babfff8
James Harris <wopian@wopian.me>
2018-04-28 03:30:54 +0100

    chore: reduce cache-control age to 15m

--- src/hibari/headers.cr
+++ src/hibari/headers.cr
@@ -4,7 +4,7 @@ module Hibari
     "Content-Type" => JsonAPI::CONTENT_TYPE,
     "Strict-Transport-Security" => "max-age=63072000; includeSubDomains; preload",
     "Content-Security-Policy" => "default-src 'none'",
-    "Cache-Control" => "max-age=86400",
+    "Cache-Control" => "max-age=900",
     "Server" => "api.hibari.moe",
     "X-Powered-By" => "api.hibari.moe",
     "X-Frame-Options" => "SAMEORIGIN",
