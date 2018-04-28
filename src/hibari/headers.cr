9823c91c6d05c16d94ac2ea80ca495d0d1680369
James Harris <wopian@wopian.me>
2018-04-28 02:38:08 +0100

    chore: enforce HSTS

--- src/hibari/headers.cr
+++ src/hibari/headers.cr
@@ -2,6 +2,7 @@ module Hibari
   # Response headers for the Hibari API
   HEADERS = {
     "Content-Type" => JsonAPI::CONTENT_TYPE,
+    "Strict-Transport-Security" => "max-age=63072000; includeSubDomains; preload",
     "X-Powered-By" => "", # TODO: Remove this header entirely
     "X-Frame-Options" => "SAMEORIGIN",
     "X-Xss-Protection" => "1; mode=block",
